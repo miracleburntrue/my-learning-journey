ff9f5be27150c491982ff206cb328745b49bd702
burn true <1255646088@qq.com>
2026-07-24 06:29:49 +0800

    feat:优化了练习2的写法

--- 01-python从零到源码/数据结构/练习2.py
+++ 01-python从零到源码/数据结构/练习2.py
@@ -6,9 +6,6 @@ str = "the quick brown fox jumps over the lazy dog the end"
 str = str.split()
 dict = {}
 for word in str:
-    if word in dict:
-        dict[word] += 1
-    else:
-        dict[word] = dict.get(word,1)
+    dict[word] = dict.get(word, 0) + 1
 
 print(dict)
