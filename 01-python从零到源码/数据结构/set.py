a5623856e62ef4a54b4e679fad549305247a3ac5
burn true <1255646088@qq.com>
2026-07-24 16:01:06 +0800

    feat:集合的基础知识汇总

---
+++ 01-python从零到源码/数据结构/set.py
@@ -0,0 +1,25 @@
+'''
+集合的基础知识汇总
+'''
+#集合是无序，不重复的数据容器
+#创建
+set_a = {1,2,3,4,5} #集合可以用{}创建
+set_empty = set() #空集合只能用set()函数创建，因为空{}创建的是空字典
+#set_b = set([2,3,4,5,6])  # set()只接受一个可迭代对象，多个元素要包在列表/元组里传进去
+set_b = set([2,3,4,5,6]) #noqa
+set_a = {x for x in range(3,8)} #集合也可以用推导式创建
+print(set_a)
+print(set_b)
+#可以用set()简单的去重
+list = [1,1,2,2,3,3,4,4,5,5]
+unique = set(list)
+print(unique)
+#集合的运算
+print(set_a & set_b) #交
+print(set_a | set_b) #并
+print(set_a - set_b) #差
+print(set_a ^ set_b) #对称差
+#增删方法
+set_empty.add(1) #增
+set_empty.discard(2) #删，不存在不会报错
+set_empty.remove(1) #删，不存在会报错
